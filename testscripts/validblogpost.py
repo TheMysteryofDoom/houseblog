6e7e249c55ac437b8ea6a5641dee5e321d326d18
gregcabrera422 <gregcabrera422@gmail.com>
2019-05-25 17:22:29 +0800

    added my scripts (#26)

---
+++ testscripts/validblogpost.py
@@ -0,0 +1,37 @@
+#This was made by Greg Marvin T. Cabrera
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("http://localhost:8080")
+
+driver.find_element_by_id("login").click()
+
+driver.save_screenshot("login_screenshot.png")
+
+# elements
+name = driver.find_element_by_id("username");
+password = driver.find_element_by_id("password");
+login = driver.find_element_by_id("login");
+
+name.clear()
+password.clear()
+
+name.send_keys("greg")
+password.send_keys("password01")
+
+driver.save_screenshot("login_credentials_screenshot.png")
+
+login.click()
+
+driver.save_screenshot("homepage_screenshot.png")
+
+driver.find_element_by_id("title").send_keys("this is my new title")
+driver.find_element_by_id("content").send_keys("this is my new content")
+
+driver.save_screenshot("blog_post_credentials_screenshot.png")
+
+driver.find_element_by_id("post").click()
+
+driver.save_screenshot("valid_blog_post_screenshot.png")
+#This was made by Greg Marvin T. Cabrera
