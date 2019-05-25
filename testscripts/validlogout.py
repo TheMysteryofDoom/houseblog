613694b9aa035d9bf7eaef8c84d1be7fe4a7394e
Dennis Rocha <30545399+DenDenRoc@users.noreply.github.com>
2019-05-25 18:24:51 +0900

    Add files via upload
    myscript

--- testscripts/validlogout.py
+++ testscripts/validlogout.py
@@ -1,32 +1,17 @@
-#This was made by Greg Marvin T. Cabrera
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome()
 driver.get("http://localhost:8080")
-
-driver.find_element_by_id("login").click()
-
-driver.save_screenshot("login_screenshot.png")
-
-# elements
-name = driver.find_element_by_id("username");
-password = driver.find_element_by_id("password");
-login = driver.find_element_by_id("login");
-
-name.clear()
-password.clear()
-
-name.send_keys("greg")
-password.send_keys("password01")
-
-driver.save_screenshot("login_credentials_screenshot.png")
-
-login.click()
-
-driver.save_screenshot("homepage_screenshot.png")
-
-driver.find_element_by_id("logout").click()
-
-driver.save_screenshot("valid_logout_screenshot.png")
-#This was made by Greg Marvin T. Cabrera
+loginButton = driver.find_element_by_id("login")
+loginButton.click()
+searchbar = driver.find_element_by_id("username")
+searchbar.clear()
+searchbar.send_keys("DenDen")
+searchbar = driver.find_element_by_id("password")
+searchbar.clear()
+searchbar.send_keys("thisisdennis")
+loginButton = driver.find_element_by_id("login")
+loginButton.click()
+loginButton = driver.find_element_by_id("logout")
+loginButton.click()
